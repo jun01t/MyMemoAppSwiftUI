ca51860239463be8c5b9e0ad74b1e4ee709fddfc
TaharaNoNeko <tmdjnch0901@gmail.com>
2021-05-29 12:12:22 +0900

    Initial Commit

---
+++ MyMemoApp/MyMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMemoAppApp.swift
+//  MyMemoApp
+//
+//  Created by Junichi Tomida on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct MyMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
